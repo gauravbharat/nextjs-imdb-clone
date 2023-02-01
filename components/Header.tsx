9cbbd017c63e8ad31e29768f491a3da1405a3828
Gaurav Bharat <gaurav.mendse@gmail.com>
2023-02-01 08:58:45 +0530

    style header section

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,15 +8,23 @@ import UserIcon from "./ui/icons/UserIcon";
 
 const Header = () => {
   return (
-    <header>
-      <div className="">
+    // add select-none to disable selecting text/icon
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="Home" />
         <HeaderIcon Icon={UserIcon} title="Account" />
         <HeaderIcon Icon={PhoneIcon} title="Contact" />
         <HeaderIcon Icon={AboutIcon} title="About" />
       </div>
-      <Image src={imdbLogo} alt="IMDB logo" width={100} height={100} />
-    </header>
+      <Image
+        style={{ borderRadius: "10px" }}
+        src={imdbLogo}
+        alt="IMDB logo"
+        width={100}
+        height={100}
+        className="cursor-pointer active:brightness-110"
+      />
+    </div>
   );
 };
 
--- components/HeaderIcon.tsx
+++ components/HeaderIcon.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const HeaderIcon = (props: any) => {
   return (
-    <div>
-      <props.Icon customClass={"h-8"} />
-      <p>{props.title}</p>
+    <div className="mx-4 flex flex-col hover:text-white active:text-red-400 cursor-pointer lg:mx-6">
+      <props.Icon customClass="h-8 " />
+      <p className="my-2">{props.title}</p>
     </div>
   );
 };
