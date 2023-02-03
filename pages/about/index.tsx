2ae84baf4f52c0551754ebfefe4d5c68a950b57e
Gaurav Bharat <gaurav.mendse@gmail.com>
2023-02-03 21:23:24 +0530

    added about page

---
+++ pages/about/index.tsx
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+  return (
+    <div className="pl-9 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        This clone website is developed for educational purpose only and uses
+        TMDb APIs.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
